45457b4c417f99819ca29a758c28238b44909906
irman <irmanfreestyle7@gmail.com>
2019-07-18 07:03:30 +0700

    add dist

---
+++ mathgame/dist/precache-manifest.ce6168b358a236580c5bb4a9e4e743b4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://irmanfreestyle.github.io/mathgame/robots.txt"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "https://irmanfreestyle.github.io/mathgame/img/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "bff67ca0ac8f04570e8a",
+    "url": "https://irmanfreestyle.github.io/mathgame/css/chunk-vendors.4e0b9ca4.css"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "cf141cbbccef87bbd74e",
+    "url": "https://irmanfreestyle.github.io/mathgame/js/app.f93077de.js"
+  },
+  {
+    "revision": "bff67ca0ac8f04570e8a",
+    "url": "https://irmanfreestyle.github.io/mathgame/js/chunk-vendors.350700eb.js"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "https://irmanfreestyle.github.io/mathgame/img/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "https://irmanfreestyle.github.io/mathgame/img/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "7e82f8b66fd21cda00bc6677ad92cff6",
+    "url": "https://irmanfreestyle.github.io/mathgame/img/logo.7e82f8b6.png"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "https://irmanfreestyle.github.io/mathgame/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "329f479af7858122138ac0281d0e4cc7",
+    "url": "https://irmanfreestyle.github.io/mathgame/index.html"
+  },
+  {
+    "revision": "cf141cbbccef87bbd74e",
+    "url": "https://irmanfreestyle.github.io/mathgame/css/app.6f697201.css"
+  }
+];
